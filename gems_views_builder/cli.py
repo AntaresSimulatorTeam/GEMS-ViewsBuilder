707a75a4149144c74cc1dcf29fbac1e83ad3f3eb
guilpierre <guillaume.pierre_externe@rte-france.com>
2026-08-14 10:10:12 +0200

    Trial to fix CI

--- gems_views_builder/cli.py
+++ gems_views_builder/cli.py
@@ -19,7 +19,7 @@ class PathOption:
     name: str
     system_type: SystemType
     system_check: Callable[[Path], bool]
-    args_attribute: str | None = None
+    args_attribute: str = ""
 
     def __post_init__(self) -> None:
         if not self.args_attribute:
