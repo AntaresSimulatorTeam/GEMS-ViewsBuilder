ae0d87dd206fd9a1e87a46100817677e7d0c0475
guilpierre <guillaume.pierre_externe@rte-france.com>
2026-08-14 10:07:19 +0200

    trial to fix CI

--- gems_views_builder/cli.py
+++ gems_views_builder/cli.py
@@ -5,7 +5,7 @@
 
 import argparse
 from collections.abc import Callable
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from enum import Enum
 from pathlib import Path
 
