ea2fb8f6c590776ffc61c4dcc7435fd5af323993
Dusan Paripovic <dusan.paripovic@rte-international.com>
2026-08-25 14:33:22 +0200

    refactor(view processing): update grouping and accumulation functions for metric views
    - Changed the return type of group_by_time_granularity to return a list of lists, simplifying the structure.
    - Renamed merge_same_granularity to accumulate_views for clarity and updated its implementation.
    - Adjusted accumulate_on_disk to work with the new grouping and accumulation logic, enhancing the overall view processing flow.

--- gems_views_builder/view/view.py
+++ gems_views_builder/view/view.py
@@ -19,17 +19,17 @@ class View:
 from gems_views_builder.view.view_sinker import ViewSinker  # noqa: E402
 
 
-def group_by_time_granularity(metric_views: list[MetricView]) -> dict[TimeGranularity, list[MetricView]]:
+def group_by_time_granularity(metric_views: list[MetricView]) -> list[list[MetricView]]:
     views_by_time_granularity: dict[TimeGranularity, list[MetricView]] = defaultdict(list)
     for view in metric_views:
         views_by_time_granularity[view.time_granularity].append(view)
-    return views_by_time_granularity
+    return list(views_by_time_granularity.values())
 
 
-def merge_same_granularity(views_by_specific_granularity: list[MetricView]) -> pl.LazyFrame:
-    return pl.scan_parquet([v.persistence_path for v in views_by_specific_granularity])
+def accumulate_views(views: list[MetricView]) -> pl.LazyFrame:
+    return pl.scan_parquet([v.persistence_path for v in views])
 
 
 def accumulate_on_disk(metric_views: list[MetricView], sinker: ViewSinker) -> None:
-    for time_granularity, views in group_by_time_granularity(metric_views).items():
-        sinker.sink(merge_same_granularity(views), time_granularity)
+    for views in group_by_time_granularity(metric_views):
+        sinker.sink(accumulate_views(views), views[0].time_granularity)
