ce087ccbf4c899b1925bcdcce9012b36dbca511d
Dusan Paripovic <dusan.paripovic@rte-international.com>
2026-08-25 17:17:48 +0200

    refactor(pattern aggregator): streamline initialization and enhance clarity
    - Updated the PatternAggregator class to initialize its attributes directly from the AggregationPattern.
    - Simplified the aggregations_factory function for creating PatternAggregator instances, improving code readability and maintainability.
    - Removed unnecessary boilerplate code in the initialization process.

--- gems_views_builder/aggregators/pattern_aggregator.py
+++ gems_views_builder/aggregators/pattern_aggregator.py
@@ -1,30 +1,26 @@
 # Copyright 2007-2026, RTE (https://www.rte-france.com)
 # SPDX-License-Identifier: MPL-2.0
 
-from dataclasses import dataclass
 
 from gems_views_builder.aggregators.scenario_aggregator import ScenarioAggregator, make_scenario_operator
-from gems_views_builder.input.view_config import TimeGranularity, ViewConfig
+from gems_views_builder.input.view_config import AggregationPattern, TimeGranularity, ViewConfig
 from gems_views_builder.metric_view import MetricView
 from gems_views_builder.spatial_filter import SpatialFilter
 
 
-@dataclass
 class PatternAggregator:
-    time_granularity: TimeGranularity
-    scenario_aggregator: ScenarioAggregator
+    def __init__(self, pattern: AggregationPattern):
+        self.time_granularity = pattern.time_granularity
+        self.scenario_aggregator = ScenarioAggregator(
+            make_scenario_operator(pattern.scenario), SpatialFilter(pattern.spatial_filter)
+        )
 
     def run(self, time_aggregated_metric_views: dict[TimeGranularity, MetricView]) -> MetricView:
         return self.scenario_aggregator.run(time_aggregated_metric_views[self.time_granularity])
 
 
 def aggregations_factory(view_config: ViewConfig) -> list[PatternAggregator]:
-    return [
-        PatternAggregator(
-            time_granularity=pattern.time_granularity,
-            scenario_aggregator=ScenarioAggregator(
-                make_scenario_operator(pattern.scenario), SpatialFilter(pattern.spatial_filter)
-            ),
-        )
-        for pattern in view_config.aggregation_patterns
-    ]
+    pattern_aggregators = []
+    for pattern in view_config.aggregation_patterns:
+        pattern_aggregators.append(PatternAggregator(pattern))
+    return pattern_aggregators
