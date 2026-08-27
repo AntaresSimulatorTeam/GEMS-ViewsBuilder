85b8444d4cf3afa788167251826a5acb2887577c
Dusan Paripovic <dusan.paripovic@rte-international.com>
2026-08-27 09:49:03 +0200

    refactor(apply spatial filter): tiny refactoring

--- gems_views_builder/spatial_filter.py
+++ gems_views_builder/spatial_filter.py
@@ -12,7 +12,6 @@ class SpatialFilter:
 
 
 def apply_spatial_filter(metric_view: pl.LazyFrame, spatial_filter: SpatialFilter) -> pl.LazyFrame:
-    if spatial_filter.locations is None:
-        return metric_view
-
-    return metric_view.filter(pl.col("metric_location").is_in(spatial_filter.locations))
+    if spatial_filter.locations:
+        return metric_view.filter(pl.col("metric_location").is_in(spatial_filter.locations))
+    return metric_view
