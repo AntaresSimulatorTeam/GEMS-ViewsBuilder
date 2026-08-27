a25d5b39807e39650819d16f96787f0e2c4f87d1
guilpierre <guillaume.pierre_externe@rte-france.com>
2026-08-27 17:42:52 +0200

    Move code related to aggregation patterns from class AgggregationProcessor into class AggregationPatternList

--- gems_views_builder/aggregators/pattern_aggregator.py
+++ gems_views_builder/aggregators/pattern_aggregator.py
@@ -3,8 +3,10 @@
 
 
 from gems_views_builder.aggregators.scenario_aggregator import ScenarioAggregator, make_scenario_operator
+from gems_views_builder.aggregators.time_aggregator import perform_time_aggregations
 from gems_views_builder.input.view_config import AggregationPattern, TimeGranularity, ViewConfig
-from gems_views_builder.metric_view import MetricView
+from gems_views_builder.input.catalog import Metric
+from gems_views_builder.metric_view import MetricView, TemporalMetricView
 from gems_views_builder.spatial_filter import SpatialFilter
 
 class PatternAggregator:
@@ -22,8 +24,15 @@ class AggregationPatternList:
     def __init__(self, view_config: ViewConfig) -> None:
         self.aggregation_patterns: list[PatternAggregator] = []
         self.time_aggregated_metric_views: dict[TimeGranularity, MetricView] = {}
+        self.time_granularities = view_config.get_time_granularities()
+
         for pattern in view_config.aggregation_patterns:
             self.aggregation_patterns.append(PatternAggregator(pattern))
 
-    def __iter__(self):
-        return iter(self.aggregation_patterns)
+    def run(self, metric: Metric, metric_view: TemporalMetricView) -> list[MetricView]:
+        metric_views: list[MetricView] = []
+        self.time_aggregated_metric_views = perform_time_aggregations(metric, metric_view, self.time_granularities)
+        for pattern in self.aggregation_patterns:
+            metric_view = pattern.run(self.time_aggregated_metric_views)
+            metric_views.append(metric_view)
+        return  metric_views
