b8a3abb4d736f0abd0c5f850b55b8cd4a69f3668
guilpierre <guillaume.pierre_externe@rte-france.com>
2026-08-27 14:43:37 +0200

    Renaming only

--- gems_views_builder/aggregators/aggregations_processor.py
+++ gems_views_builder/aggregators/aggregations_processor.py
@@ -4,18 +4,19 @@
 
 import polars as pl
 
-from gems_views_builder.aggregators.pattern_aggregator import aggregations_factory
+from gems_views_builder.aggregators.pattern_aggregator import aggregation_patterns_factory
 from gems_views_builder.aggregators.terms_aggregator import TermsAggregator
 from gems_views_builder.aggregators.time_aggregator import perform_time_aggregations
 from gems_views_builder.input.catalog import Metric
-from gems_views_builder.input.view_config import ViewConfig
+from gems_views_builder.input.view_config import ViewConfig, TimeGranularity
 from gems_views_builder.metric_view import MetricView
 
 
 class AgggregationProcessor:
     def __init__(self, view_config: ViewConfig) -> None:
         self.terms_aggregator = TermsAggregator()
-        self.aggregations = aggregations_factory(view_config)
+        # self.time_aggregated_metric_views = dict[TimeGranularity, MetricView]
+        self.aggregation_patterns = aggregation_patterns_factory(view_config)
         self.time_granularities = view_config.get_time_granularities()
 
     def run(self, structured_simulation_table: pl.LazyFrame, metric: Metric) -> list[MetricView]:
@@ -24,7 +25,7 @@ class AgggregationProcessor:
 
         time_aggregated_metric_views = perform_time_aggregations(metric, metric_view, self.time_granularities)
 
-        for aggregation in self.aggregations:
-            metric_view = aggregation.run(time_aggregated_metric_views)
+        for pattern in self.aggregation_patterns:
+            metric_view = pattern.run(time_aggregated_metric_views)
             metric_views.append(metric_view)
         return metric_views
--- gems_views_builder/aggregators/pattern_aggregator.py
+++ gems_views_builder/aggregators/pattern_aggregator.py
@@ -19,7 +19,7 @@ class PatternAggregator:
         return self.scenario_aggregator.run(time_aggregated_metric_views[self.time_granularity])
 
 
-def aggregations_factory(view_config: ViewConfig) -> list[PatternAggregator]:
+def aggregation_patterns_factory(view_config: ViewConfig) -> list[PatternAggregator]:
     pattern_aggregators = []
     for pattern in view_config.aggregation_patterns:
         pattern_aggregators.append(PatternAggregator(pattern))
