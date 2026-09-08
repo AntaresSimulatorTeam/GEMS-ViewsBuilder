e7b1e6a257affee75cb4ddf2cdca04b8e7d3a594
Dusan Paripovic <dusan.paripovic@rte-international.com>
2026-09-08 16:47:50 +0200

    refactor(validation): improve view config metric validation logic
    - Updated the `ViewConfigMetricsAgainstCatalogsValidator` to directly use `self.view_config.id` instead of storing it in a separate variable, enhancing clarity.
    - Renamed the `validate_used_metric_ids` method to `_validate_metric_ids` for better consistency with naming conventions.
    - Adjusted error messages to reference `self.view_config.id` directly, improving the accuracy of validation feedback.

--- gems_views_builder/validation/view_config_metrics_against_catalogs_validator.py
+++ gems_views_builder/validation/view_config_metrics_against_catalogs_validator.py
@@ -9,22 +9,21 @@ from gems_views_builder.input.view_config import ViewConfig
 class ViewConfigMetricsAgainstCatalogsValidator:
     def __init__(self, catalogs: list[Catalog], view_config: ViewConfig):
         self.view_config = view_config
-        self.view_config_id = view_config.id
         self.catalogs = {catalog.id: catalog for catalog in catalogs}
 
     def validate(self) -> None:
-        logging.info(f"Validating view config {self.view_config_id!r} metrics against {len(self.catalogs)} catalog(s)")
-        self.validate_used_metric_ids()
+        logging.info(f"Validating view config {self.view_config.id!r} metrics against {len(self.catalogs)} catalog(s)")
+        self._validate_metric_ids()
         logging.info("All metrics are present in the catalogs")
 
-    def validate_used_metric_ids(self) -> None:
+    def _validate_metric_ids(self) -> None:
         for metric_ref in self.view_config.metric_ids:
             catalog_id, metric_id = metric_ref.split(".")
 
             if catalog_id not in self.catalogs:
-                raise ValueError(f"Catalog {catalog_id} used in view config {self.view_config_id!r} doesn't exist!")
+                raise ValueError(f"Catalog {catalog_id} used in view config {self.view_config.id!r} doesn't exist!")
 
             if metric_id not in self.catalogs[catalog_id].metrics:
                 raise ValueError(
-                    f"Metric {metric_id} used in view config {self.view_config_id!r} doesn't exist in catalog {catalog_id!r}"
+                    f"Metric {metric_id} used in view config {self.view_config.id!r} doesn't exist in catalog {catalog_id!r}"
                 )
