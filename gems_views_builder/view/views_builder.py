06ab0aa8a98aeaf6c1919c5ec08d01866e64852d
guilpierre <guillaume.pierre_externe@rte-france.com>
2026-09-14 18:32:57 +0200

    Just a bit more clarity

--- gems_views_builder/view/views_builder.py
+++ gems_views_builder/view/views_builder.py
@@ -9,7 +9,7 @@ from gems_views_builder.metric_view import TemporalMetricView
 from gems_views_builder.metrics_structure_builder import MetricStructureTableBuilder
 
 
-def give_views_a_config(metric_views: list[TemporalMetricView], view_config: ViewConfig) -> None:
+def give_views_a_config_id(metric_views: list[TemporalMetricView], view_config: ViewConfig) -> None:
     for view in metric_views:
         view.view_config_id = view_config.id
 
@@ -20,17 +20,17 @@ class ViewBuilder:
         metric_structure_table_builder: MetricStructureTableBuilder,
         aggregation_processor: AgggregationProcessor,
     ) -> None:
-        self.input_data = input_data
+        self.filtered_st = input_data.filtered_st
+        self.view_config = input_data.view_config
         self.metric_structure_table_builder = metric_structure_table_builder
         self.aggregation_processor = aggregation_processor
-        self.view_config = input_data.view_config
 
     def build(self) -> list[TemporalMetricView]:
         metric_views: list[TemporalMetricView] = []
         for metric in self.view_config.metrics:
             metric_structure_table = self.metric_structure_table_builder.build(metric)
-            structured_simulation_table = join(metric_structure_table, self.input_data.filtered_st)
+            structured_simulation_table = join(metric_structure_table, self.filtered_st)
             views = self.aggregation_processor.run(structured_simulation_table, metric)
             metric_views.extend(views)
-        give_views_a_config(metric_views, self.view_config)
+        give_views_a_config_id(metric_views, self.view_config)
         return metric_views
